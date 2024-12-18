ca757f94f46bf141dfa0732023d0045b7ab47348
uragan125 <asafrileev@gmail.com>
2024-12-18 17:27:11 +0600

    Готовый проект 2

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <!--Footer-->
-<h6 class="text-center bg-dark text-white p-3 m-0">Разработано и создано HBWDevops</h6>
+<h6 class="text-center bg-dark text-white p-3 m-0">Сайт разработал Сафрилеев Артем, студент 942 группы</h6>
 <!--Footer-->
